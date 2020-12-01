573ff460d3d5ad1457f3c1a263334560fcfd1e6c
Maria Mathew <mariamathewc@gmail.com>
2020-12-01 12:00:37 -0800

    Updates

---
+++ precache-manifest.e221b0fcab5b25437f3bba8db3ba0bd5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ffc5ff37d677ed46e9ccd1835205116",
+    "url": "/seainfo6150-final-project-webapp/index.html"
+  },
+  {
+    "revision": "7a74d924c7a97f557502",
+    "url": "/seainfo6150-final-project-webapp/static/css/main.dc109e2b.chunk.css"
+  },
+  {
+    "revision": "d164d251ca16affa5c86",
+    "url": "/seainfo6150-final-project-webapp/static/js/2.8525507b.chunk.js"
+  },
+  {
+    "revision": "7a74d924c7a97f557502",
+    "url": "/seainfo6150-final-project-webapp/static/js/main.30c8f611.chunk.js"
+  },
+  {
+    "revision": "940ec4a42937c37ae625",
+    "url": "/seainfo6150-final-project-webapp/static/js/runtime~main.c4d7f5e8.js"
+  },
+  {
+    "revision": "1c6b87d54a8abfcc0d4d34a3e7872c80",
+    "url": "/seainfo6150-final-project-webapp/static/media/Campgrounds.1c6b87d5.jpg"
+  },
+  {
+    "revision": "295e255388cf155d65aa099e5375d61c",
+    "url": "/seainfo6150-final-project-webapp/static/media/Homepage.295e2553.jpg"
+  },
+  {
+    "revision": "6962c27173de193731c288cbf96f2098",
+    "url": "/seainfo6150-final-project-webapp/static/media/business1.6962c271.jpg"
+  },
+  {
+    "revision": "72274ad04a4dd90dbdc4a25ae022bcb4",
+    "url": "/seainfo6150-final-project-webapp/static/media/business3.72274ad0.jpg"
+  },
+  {
+    "revision": "46104736b25f8835107b33ca5268dd5d",
+    "url": "/seainfo6150-final-project-webapp/static/media/business4.46104736.jpg"
+  },
+  {
+    "revision": "3a9140d0b54dfdb8787038565558868c",
+    "url": "/seainfo6150-final-project-webapp/static/media/cover.3a9140d0.jpg"
+  },
+  {
+    "revision": "43aa975e50cc187b37951c57a3df19c4",
+    "url": "/seainfo6150-final-project-webapp/static/media/error-404.43aa975e.png"
+  },
+  {
+    "revision": "db3aa6e91bb0c3f2a35892f970479729",
+    "url": "/seainfo6150-final-project-webapp/static/media/snow.db3aa6e9.jpg"
+  },
+  {
+    "revision": "3a3e5d3aa4a951fa0836c73cca108728",
+    "url": "/seainfo6150-final-project-webapp/static/media/winter.3a3e5d3a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/seainfo6150-final-project-webapp/precache-manifest.abe1473d7e91038c58a02ec40d36d121.js"
+  "/seainfo6150-final-project-webapp/precache-manifest.e221b0fcab5b25437f3bba8db3ba0bd5.js"
 );
 
 self.addEventListener('message', (event) => {
