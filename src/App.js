61d0a19570246924d6a4e1a8ba7936a1ed9e26a3
Maria Mathew <mariamathewc@gmail.com>
2020-12-03 12:40:04 -0800

    HTML: Validate html for Travel and Tents

--- src/App.js
+++ src/App.js
@@ -22,15 +22,6 @@ import ThankYou from './components/Products/ThankYou.jsx';
 
 
 
-// here is some external content. look at the /baz route below
-// to see how this content is passed down to the components via props
-const externalContent = {
-    id: "article-1",
-    title: "An Article",
-    author: "April Bingham",
-    text: "Some text in the article",
-};
-
 function App() {
 
     const [fetchedData, setFetchedData] = useState();
